5106857433b45e7cb895840ead2600dd7761d162
AmyShang-alt <58060647+AmyShang-alt@users.noreply.github.com>
2021-08-16 15:38:25 +0800

    Update file_util.py
    支持Windows、Mac不同路径存储数据

--- util/file_util.py
+++ util/file_util.py
@@ -6,13 +6,21 @@
 # @Software: PyCharm
 # @contact: xinyu.shang@zozo.cn
 from typing import List, Tuple
-
+import platform, os
 import xlsxwriter
 
 
 def save_excel(file_name: str, sheet_data: List[Tuple[str, List[str], List[Tuple]]]):
+    if 'Windows' in platform.system():
+        if not os.path.exists('.\\report'):
+            os.makedirs('.\\report')
+        name = '.\\report\%s.xlsx' % file_name
+    else:
+        if not os.path.exists('./report'):
+            os.makedirs('./report')
+        name = './report/%s.xlsx' % file_name
     workbook = xlsxwriter.Workbook(
-        filename='./report/%s.xlsx' % file_name)
+        filename=name)
     for sheet_name, headers, data_list in sheet_data:
         st = workbook.add_worksheet(sheet_name)
         for i in range(0, len(headers)):
